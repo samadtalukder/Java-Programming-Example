f9d150d4643e8172336e4699973d5272a7eac33c
samadtalukder <samadtalukdar6@gmail.com>
2018-05-18 17:01:37 +0600

    Math.sqrt program

--- Java Programming Example/src/com/misc/program/DynamicInitial.java
+++ Java Programming Example/src/com/misc/program/DynamicInitial.java
@@ -7,8 +7,8 @@ public class DynamicInitial {
 	public static void main(String[] args) {
 		// 
 		double a = 3.0, b = 4.0;
-//		double result;
-		double result=Math.sqrt(a*a + b*b);
+		//	double result;
+		double result = Math.sqrt(a*a + b*b);
 		System.out.println("Square : "+result);
 		
 
