29d5ec926dc92363e1ecca352f7af94f9452854d
samadtalukder <samadtalukdar6@gmail.com>
2018-05-18 17:01:16 +0600

    Class Variable Program

---
+++ Java Programming Example/src/com/misc/program/ClassVariable.java
@@ -0,0 +1,20 @@
+package com.misc.program;
+/*Code By: 
+ * Samad Talukder 
+ */
+public class ClassVariable {
+	
+	String hello=new String("Hello");
+	
+	void sayHello()
+	{
+		System.out.println(hello);
+	}
+	
+	public static void main(String[] args) {
+		// 
+		ClassVariable obj=new ClassVariable();
+		obj.sayHello();
+	}
+
+}
