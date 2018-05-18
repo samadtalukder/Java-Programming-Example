663b5cbd4512d80661dc0fcd0e171e1ed5f17052
samadtalukder <samadtalukdar6@gmail.com>
2018-05-18 16:38:57 +0600

    Display Time Program

---
+++ Java Programming Example/src/com/misc/program/DisplayTime.java
@@ -0,0 +1,18 @@
+package com.misc.program;
+import java.util.Scanner;
+
+public class DisplayTime {
+
+	public static void main(String[] args) {
+		Scanner inp= new Scanner(System.in);
+		System.out.println("Enter Seconds: ");
+		int seconds=inp.nextInt();
+		
+		int minutes= seconds / 60;
+		int reamingSeconds=seconds % 60;
+		System.out.println(minutes+" Minutes reaming "+reamingSeconds+" seconds");
+		
+
+	}
+
+}
