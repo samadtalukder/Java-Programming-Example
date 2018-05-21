067a403b7af693601b38b3627d731ddec09c236d
pixel <pixel@pi-lab>
2018-05-21 17:40:13 +0600

    Static Example

---
+++ src/com/static_/StaticMethodExample.java
@@ -0,0 +1,22 @@
+package com.static_;
+
+public class StaticMethodExample {
+
+	public static void main(String[] args) {
+		int result = Utitlity.add(25, 25);
+		System.out.println("Result: "+result);
+		
+		double divided = Utitlity.divided(10, 5);
+		System.out.println("Divided: "+divided); 
+		
+	}
+
+}
+class Utitlity{
+	public static int add(int first,int second) {
+		return first + second;
+	}
+	public static double divided(double first, double second) {
+		return first / second;
+	}
+}
---
+++ src/com/static_/StaticMethodVariableExample.java
@@ -0,0 +1,32 @@
+package com.static_;
+
+public class StaticMethodVariableExample {
+
+	public static void main(String[] args) {
+		University uni = new University();
+		uni.showData();
+		University uni2 = new University();
+		uni2.showData();
+		University uni3 = new University();
+		uni3.showData();
+		int inc = University.increment();
+		System.out.println("Inc:"+inc);
+	}
+
+}
+class University{
+	int a;
+	static int b;
+	public University() {
+		b++;
+	}
+	public void showData() {
+		System.out.println("Value of A: "+a);
+		System.out.println("Value of B: "+b);
+	}
+	public static int increment() {
+		 return b++;
+	}
+	
+	
+}
