2a82347b4a2a80d1a469884632e0f55f537e773a
pixel <pixel@pi-lab>
2018-05-21 16:32:15 +0600

    Simple For Loop Example

---
+++ src/com/loop/SimpleForLoop.java
@@ -0,0 +1,17 @@
+package com.loop;
+
+public class SimpleForLoop {
+
+	public static void main(String[] args) {
+		int num = 50;
+		for(int i = 0; i< 50; i++) {
+			System.out.println(i);
+		}
+		for(int i = 0; i<= num; i++) {
+			System.out.println("L: "+i);
+		}
+		
+		
+	}
+
+}
---
+++ src/com/loop/SumOfNaturalNumber.java
@@ -0,0 +1,14 @@
+package com.loop;
+
+public class SumOfNaturalNumber {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i <= 100;i++) {
+			sum += i;
+		}
+		System.out.println("Sum Of Natural Number: "+sum);
+
+	}
+
+}
