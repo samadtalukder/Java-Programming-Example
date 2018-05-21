026054a05ed5add8cab659d0df9677a1be23ec54
pixel <pixel@pi-lab>
2018-05-21 17:46:33 +0600

    Switch Statement Example

---
+++ src/com/conditional/SwitchStatementExample.java
@@ -0,0 +1,26 @@
+package com.conditional;
+
+public class SwitchStatementExample {
+
+	public static void main(String[] args) {
+		int num = 3;
+		switch (num) {
+		case 1:
+			System.out.println("Fan On");
+			break;
+		case 2:
+			System.out.println("Light On");
+			break;
+		case 3:
+			System.out.println("Lamp On");
+			break;
+		case 4:
+			System.out.println("Dim Light On");
+			break;
+		default:
+			System.out.println("All Off");
+			break;
+		}
+	}
+
+}
