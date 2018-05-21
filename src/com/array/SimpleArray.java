2264460a01850d8f5a36765d3a6d60b03eea715c
pixel <pixel@pi-lab>
2018-05-21 16:33:23 +0600

    Simple Array Program

--- src/com/array/SimpleArray.java
+++ src/com/array/SimpleArray.java
@@ -3,9 +3,15 @@ package com.array;
 public class SimpleArray {
 
 	public static void main(String[] args) {
-		int[] arr = {25,34,65};
+		int[] arr = {25,65,58,96,87,52,541};
+		
 		for(int i =0; i<arr.length;i++) {
-			System.out.println(arr[i]);
+			if (arr[i] % 2 != 0) {
+				System.out.println("Odd Numbers: "+arr[i]);
+			}
+			else {
+				System.out.println("Even Numbers: "+arr[i]);
+			}
 		}
 
 	}
