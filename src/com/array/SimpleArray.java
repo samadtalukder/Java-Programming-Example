fa9f8b238bf68a0c75d409a2c495f4f9c674ce70
pixel <pixel@pi-lab>
2018-05-21 15:34:08 +0600

    updated

---
+++ src/com/array/SimpleArray.java
@@ -0,0 +1,13 @@
+package com.array;
+
+public class SimpleArray {
+
+	public static void main(String[] args) {
+		int[] arr = {25,34,65};
+		for(int i =0; i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
