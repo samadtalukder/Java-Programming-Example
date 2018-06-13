2c26b573065469e04d14b9d93e8d8cac1d7fedcc
samadtalukder <samadtalukdar6@gmail.com>
2018-06-13 14:18:13 +0600

    loop and data type example

---
+++ src/com/loop/LoopEx1.java
@@ -0,0 +1,15 @@
+package com.loop;
+
+public class LoopEx1 {
+
+	public static void main(String[] args) {
+		int num = 15;
+		for (int i = 0; i<10; i++) {
+			System.out.println("i: "+i);
+			System.out.println("num: "+num);
+			num -= 1;
+		}
+
+	}
+
+}
