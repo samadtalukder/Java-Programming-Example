6fa664502e3cc341da062ce92e6ee82b7765b445
samadtalukder <samadtalukdar6@gmail.com>
2018-08-16 18:19:14 +0600

    Array Length

---
+++ src/com/array/ArrayLength.java
@@ -0,0 +1,15 @@
+package com.array;
+
+public class ArrayLength {
+
+	public static void main(String[] args) {
+		// declare an array
+		int arr[]= {10,20,30,50};
+		// declare array length
+		int arrLength = arr.length;
+		// print array length
+		System.out.println("The Size of Array: "+arrLength);
+
+	}
+
+}
