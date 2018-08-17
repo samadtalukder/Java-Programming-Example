58944471a80d0fd1ebe71a1d755d8d04bb34eced
samadtalukder <samadtalukdar79@gmail.com>
2018-08-17 12:43:43 +0600

    Compound assignment operator

---
+++ src/com/operator/CompoundAssignmentOperator.java
@@ -0,0 +1,27 @@
+package com.operator;
+
+public class CompoundAssignmentOperator {
+
+	public static void main(String[] args) {
+		// var = var op expression;
+		// var op = expression;
+		int num1 = 74;
+		int num2 = 69;
+		int num3 = 33;
+		int num4 = 55;
+		int num5 = 20;
+		
+		num1 += 6;
+		num2 -= 9;
+		num3 *= 3;
+		num4 /= 5;
+		num5 %= 3;
+		
+		System.out.println(""+num1);
+		System.out.println(""+num2);
+		System.out.println(""+num3);
+		System.out.println(""+num4);
+		System.out.println(""+num5);
+	}
+
+}
