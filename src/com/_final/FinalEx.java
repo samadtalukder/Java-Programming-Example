bd2d23c1b5dd89c634ea780e0191dd0c4217c1e7
samadtalukder <samadtalukdar6@gmail.com>
2019-03-09 18:21:11 +0600

    final program added

--- src/com/_final/FinalEx.java
+++ src/com/_final/FinalEx.java
@@ -1,4 +1,18 @@
 package com._final;
 
 public class FinalEx {
+    // final variable direct initialize
+    final int NUM = 5;
+    // blank final variable
+    // final int CAPACITY;
+
+    // final static variable direct initialize
+    static final double PI = 3.141592;
+
+    public static void main(String[] args) {
+        int arr[] = {2,1,5,6};
+        for (final int i:arr){
+            System.out.println(i);
+        }
+    }
 }
